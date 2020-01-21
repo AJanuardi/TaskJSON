1c3726a75a34865c632cf07daf332ff4a162f2f5
Agung Januardi <agung.januardi@bsi.co.id>
2020-01-21 13:49:49 +0700

    Push ulang karena tidak sengaja ter-replace dengan TaskLINQ

---
+++ JSON1/Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace JSONtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             string json = @"[{
+                                ""id"": 323,
+                                ""username"": ""rinood30"",
+                                ""profile"": {
+                                ""full_name"": ""Shabrina Fauzan"",
+                                ""birthday"": ""1988-10-30"",
+                                ""phones"": [
+                                    ""08133473821"",
+                                    ""082539163912"",
+                                ],
+                                },
+                                ""articles"": [
+                                {
+                                    ""id"": 3,
+                                    ""title"": ""Tips Berbagi Makanan"",
+                                    ""published_at"": ""2019-01-03T16:00:00""
+                                },
+                                {
+                                    ""id"": 7,
+                                    ""title"": ""Cara Membakar Ikan"",
+                                    ""published_at"": ""2019-01-07T14:00:00""
+                                }
+                                ]
+                            },
+                            {
+                                ""id"": 201,
+                                ""username"": ""norisa"",
+                                ""profile"": {
+                                ""full_name"": ""Noor Annisa"",
+                                ""birthday"": ""1986-08-14"",
+                                ""phones"": [],
+                                },
+                                ""articles"": [
+                                {
+                                    ""id"": 82,
+                                    ""title"": ""Cara Membuat Kue Kering"",
+                                    ""published_at"": ""2019-10-08T11:00:00""
+                                },
+                                {
+                                    ""id"": 91,
+                                    ""title"": ""Cara Membuat Brownies"",
+                                    ""published_at"": ""2019-11-11T13:00:00""
+                                },
+                                {
+                                    ""id"": 31,
+                                    ""title"": ""Cara Membuat Brownies"",
+                                    ""published_at"": ""2019-11-11T13:00:00""
+                                }
+                                ]
+                            },
+                            {
+                                ""id"": 42,
+                                ""username"": ""karina"",
+                                ""profile"": {
+                                ""full_name"": ""Karina Triandini"",
+                                ""birthday"": ""1986-04-14"",
+                                ""phones"": [
+                                    ""06133929341""
+                                ],
+                                },
+                                ""articles"": []
+                            },
+                            {
+                                ""id"": 201,
+                                ""username"": ""icha"",
+                                ""profile"": {
+                                ""full_name"": ""Annisa Rachmawaty"",
+                                ""birthday"": ""1987-12-30"",
+                                ""phones"": [],
+                                },
+                                ""articles"": [
+                                {
+                                    ""id"": 39,
+                                    ""title"": ""Tips Berbelanja Bulan Tua"",
+                                    ""published_at"": ""2019-04-06T07:00:00""
+                                },
+                                {
+                                    ""id"": 43,
+                                    ""title"": ""Cara Memilih Permainan di Steam"",
+                                    ""published_at"": ""2019-06-11T05:00:00""
+                                },
+                                {
+                                    ""id"": 58,
+                                    ""title"": ""Cara Membuat Brownies"",
+                                    ""published_at"": ""2019-09-12T04:00:00""
+                                }
+                                ]
+                            }]";
+    
+           
+           var list = JsonConvert.DeserializeObject<List<User>>(json);
+
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang tidak memiliki nomor telepon");
+           foreach(var item in list)
+           {   
+               if((item.Profile.Phones).Length == 0)
+               {
+                 Console.WriteLine(item.Username);
+               }      
+           }
+
+           
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang memiliki Artikel");
+           foreach(var item in list)
+           {   
+                if((item.articles).Count > 0)
+                {
+                    Console.WriteLine(item.Username);
+                }
+           }
+
+           
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang memiliki kata Annis pada nama");
+           foreach(var item in list)
+           {
+               if((item.Profile.Fullname).Contains("Annis"))
+               {
+                   Console.WriteLine(item.Username);
+               }
+           }
+
+           
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang lahir pada tahun 1986");
+           foreach(var item in list)
+           {
+               if((item.Profile.Birthday).Year == 1986)
+               {
+                   Console.WriteLine(item.Username);
+               }
+           }
+
+           
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang memiliki artikel pada tahun 2020");
+           foreach(var item in list)
+           {
+               foreach(var x in item.articles)
+               {
+                if((x.published_at).Year == 2020)
+                {
+                    Console.WriteLine(item.Username);
+                }
+               }
+           }
+
+           
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang memiliki judul tips");
+           foreach(var item in list)
+           {
+               foreach(var x in item.articles)
+               {
+                if((x.title).Contains("Tips"))
+                {
+                    Console.WriteLine(item.Username + " " + x.title);
+                }
+               }
+           }
+
+            
+           Console.WriteLine("======================================");
+           Console.WriteLine("User yang publish artikel sebelum Agustus 2019");
+           var filterDate = new DateTime(2019,08,01);
+           foreach(var item in list)
+           {
+               foreach(var x in item.articles)
+               {
+                if((x.published_at) < filterDate) 
+                {
+                    Console.WriteLine(item.Username + " " + x.title);
+                }
+               }
+           }
+           Console.WriteLine("======================================");    
+        }
+    }
+    public class User
+    {
+        [JsonProperty("id")]
+        public int Id {get;set;}
+        [JsonProperty("username")]
+        public string Username {get;set;}
+        public Profile Profile {get;set;}
+        public List<Articles> articles {get;set;} = null;
+
+    }
+
+    public class Profile
+    {
+        [JsonProperty("id")]
+        public int Id {get;set;}
+        [JsonProperty("full_name")]
+        public string Fullname {get;set;}
+        [JsonProperty("birthday")]
+        public DateTime Birthday {get;set;}
+        [JsonProperty("phones")]
+        public string[] Phones {get;set;} = {};
+
+    }
+
+    public class Articles
+    {
+        [JsonProperty("id")]
+        public int id {get;set;}
+        [JsonProperty("title")]
+        public string title {get;set;}
+        [JsonProperty("published_at")]
+        public DateTime published_at {get;set;}
+    }
+    
+}
---
+++ JSON3/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace JSON3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = @"[
+  {
+    ""inventory_id"": 9382,
+    ""name"": ""Brown Chair"",
+    ""type"": ""furniture"",
+    ""tags"": [
+      ""chair"",
+      ""furniture"",
+      ""brown""
+    ],
+    ""purchased_at"": 1579190471,
+    ""placement"": {
+      ""room_id"": 3,
+      ""name"": ""Sangkuriang""
+    }
+  },
+  {
+    ""inventory_id"": 9380,
+    ""name"": ""Big Desk"",
+    ""type"": ""furniture"",
+    ""tags"": [
+      ""desk"",
+      ""furniture"",
+      ""brown""
+    ],
+    ""purchased_at"": 1579190642,
+    ""placement"": {
+      ""room_id"": 3,
+      ""name"": ""Sangkuriang""
+    }
+  },
+  {
+    ""inventory_id"": 2932,
+    ""name"": ""LG Monitor 50 inch"",
+    ""type"": ""electronic"",
+    ""tags"": [
+      ""monitor""
+    ],
+    ""purchased_at"": 1579017842,
+    ""placement"": {
+      ""room_id"": 3,
+      ""name"": ""Sangkuriang""
+    }
+  },
+  {
+    ""inventory_id"": 232,
+    ""name"": ""Sharp Pendingin Ruangan 2PK"",
+    ""type"": ""electronic"",
+    ""tags"": [
+      ""ac""
+    ],
+    ""purchased_at"": 1578931442,
+    ""placement"": {
+      ""room_id"": 5,
+      ""name"": ""Dhanapala""
+    }
+  },
+  {
+    ""inventory_id"": 9382,
+    ""name"": ""Alat Makan"",
+    ""type"": ""tableware"",
+    ""tags"": [
+      ""spoon"",
+      ""fork"",
+      ""tableware""
+    ],
+    ""purchased_at"": 1578672242,
+    ""placement"": {
+      ""room_id"": 10,
+      ""name"": ""Rajawali""
+    }
+  }
+]";
+        User obj = new User();
+        var MyObj = JsonConvert.DeserializeObject<List<User>>(json);
+
+        Console.WriteLine("================================");
+        Console.WriteLine("Jumlah barang pada Sangkuriang: ");
+            int total = 0;
+            foreach (var x in MyObj)
+            {
+                if (x.Placement.Name == "Sangkuriang")
+                {
+                    total++;
+                    Console.WriteLine("Sangkuriang Room :" + x.Name);
+                }
+            }
+            Console.WriteLine("Total: " + total + " barang");
+
+        Console.WriteLine("================================");
+        Console.WriteLine("Jumlah elektronik: ");
+            int elektrik = 0;
+            foreach (var x in MyObj)
+            {
+                if (x.Type == "electronic")
+                {
+                    elektrik++;
+                }
+            }
+            Console.WriteLine("Total: " + elektrik + " barang");
+        
+        Console.WriteLine("================================");
+        Console.WriteLine("Jumlah furniture: ");
+            int furnitur = 0;
+            foreach (var x in MyObj)
+            {
+                if (x.Type == "furniture")
+                {
+                    furnitur++;
+                    Console.WriteLine("Furniture :" + x.Name);
+                }
+            }
+            Console.WriteLine("Total: " + furnitur + " barang");
+        
+        Console.WriteLine("================================");
+        Console.WriteLine("Jumlah item yang dibayar pada 16 Jan 2020: ");
+            int bayar = 0;
+            DateTime hasil = new DateTime(2020, 01, 16);
+            string tgl1 = hasil.ToShortDateString();
+            string tgl = "";
+            foreach (var x in MyObj)
+            {
+                var date = DateTimeOffset.FromUnixTimeSeconds(x.PurchasedAt).DateTime.ToShortDateString();
+                tgl += date;
+                //System.Console.WriteLine(tgl);
+                //if ((DateTime.Parse(x.PurchasedAt).ToString()).Contains("1601202)"))
+                if (tgl1 == tgl)
+                {
+                    bayar++;
+                    tgl ="";
+                    Console.WriteLine("Pembelian 16 Januari 2020 :" + x.Name);
+                }
+            }
+            Console.WriteLine("Total: " + bayar + " barang");
+        
+        Console.WriteLine("================================");
+        Console.WriteLine("Jumlah barang cokelat: ");
+            int brown = 0;
+            foreach (var x in MyObj)
+            {
+                if (x.Tags.Contains("brown"))
+                {
+                    brown++;
+                    Console.WriteLine("Furniture :" + x.Name);
+                }
+            }
+            Console.WriteLine("Total: " + furnitur + " barang");
+            Console.WriteLine("================================");
+        
+        }
+        class User
+        {
+            [JsonProperty("inventory_id")]
+            public int InveroryId {get; set;}
+            [JsonProperty("name")]
+            public string Name {get;set;}
+            [JsonProperty("type")]
+            public string Type {get; set;}
+            [JsonProperty("purchased_at")]
+            public long PurchasedAt {get; set;}
+            [JsonProperty("placement")]
+            public Placement Placement {get; set;}
+            [JsonProperty("tags")]
+            public List<string> Tags {get; set;} = new List<string>();
+        }
+        class Placement
+        {
+            [JsonProperty("room_id")]
+            public int RoomId {get; set;}
+            [JsonProperty("name")]
+            public string Name {get; set;}
+        }
+    }
+}
